589ca2bc0d9744385c1066fdcc984edb75bd58ed
Samuel Araujo <samuekatiom@gmail.com>
2022-03-30 23:34:36 -0300

    Exercicio Laço

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,33 @@
+//Exercícios de interpretação de código
+
+//--------------------01--------------------
+//Será impresso no console o valor 10
+
+//--------------------02--------------------
+// a. Será impresso 6 console.log, sendo impresso: 19,21,23,25,27,30
+// b. Sim é possível usando:
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {	
+console.log(`${numero} índice = ${lista.indexOf(numero)}`)
+}
+
+//--------------------03--------------------
+//não cosegui fazer interpretar o código
+
+//Exercícios de escrita de código
+
+//--------------------01--------------------
+
+// let bichinhosDeEstimacao = Number(prompt('Olá, quantos bichinhos de estimação você têm? Digite números por gentileza'))
+// let listaPet = []
+// let nomePet = ""
+// if(bichinhosDeEstimacao === 0){
+//     console.log('Que pena! Você pode adodar um pet!')
+// }else{
+//     while(nomePet <= 2){
+//         prompt('Digite os nomes')
+        
+//     }
+// }
+
+
