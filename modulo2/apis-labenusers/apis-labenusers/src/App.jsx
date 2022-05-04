16404ccb691ca46c61d4b6b0fe9ffb54b3593aeb
Samuel Araujo <samuekatiom@gmail.com>
2022-05-04 15:22:59 -0300

    Subindo o projeto finalizado

--- modulo2/apis-labenusers/apis-labenusers/src/App.jsx
+++ modulo2/apis-labenusers/apis-labenusers/src/App.jsx
@@ -1,102 +1,37 @@
 import React from "react";
+import "./App.css"
 import RegistrationScreen from "./components/RegistrationScreen";
-import UsersSreen from "./components/UsersScreen";
-import axios from "axios";
+import UserListScreen from "./components/UserListScreen";
 
-
-class App extends React.Component {
+export default class App extends React.Component{
   state = {
-    currentScreen: "registrationScreen",
-    inputName: "",
-    inputEmail: "",
-    usersList: [],
-    userId: "",
-
-  }
-  
-  updateInputName=(event)=>{
-    const newInputName = event.target.value;
-    this.setState({inputName: newInputName})
-  }
-  updateInputEmail=(event)=>{
-    const newInputEmail = event.target.value;
-    this.setState({inputEmail: newInputEmail})
+    currentScreen:"register"
   }
 
-  handleCreateUsers = () => {
-    const body = {
-      "name": this.state.inputName,
-      "email": this.state.inputEmail
-    };
-    const keyAuthorization = {
-      headers: {
-        Authorization: "samuel-araujo-aragon"
-      }
-    };
-
-    axios
-      .post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-        body,
-        keyAuthorization
-      )
-      .then(() => {
-        alert(`Usuário ${this.state.inputName} criado com sucesso!`);
-        this.setState({inputName: "", inputEmail: ""});
-      })
-      .catch(error =>{
-        alert('Erro ao criar o usuário');
-        console.log(error);
-      })
-  }
-  componentDidMount() {
-    this.userFinder()
-  }
-
-  userFinder = () => {
-    const keyAuthorization = {
-      headers: {
-        Authorization: "samuel-araujo-aragon"
-      }
-    };
-
-    axios
-    .get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-    keyAuthorization
-    )
-    .then(response => {
-      this.setState({usersList: response.data})
-    })
-  }
-
-  changeScreen = () => {
-    if (this.state.currentScreen === "registrationScreen") {
-      this.setState({currentScreen: "usersScreen"})
-    }else {
-      this.setState({currentScreen: "registrationScreen"})
+  chooseCanvas = () => {
+    switch (this.state.currentScreen){
+      case "register":
+        return <RegistrationScreen goList={this.goList}/>
+      case "list":
+        return <UserListScreen goRegister={this.goRegister}/>
+      default:
+        return <div>Erro! Pagina não encontrada!</div>
     }
   }
+
+  goRegister = () => {
+    this.setState({currentScreen: "register"})
+  }
+
+  goList = () => {
+    this.setState({currentScreen: "list"})
+  }
+
   render(){
     return(
-            <div>
-              <button onClick={this.changeScreen}>Alterar de Tela</button>
-              {
-                this.state.currentScreen === "registrationScreen"
-                  ?<RegistrationScreen
-                  inputName= {this.state.inputName}
-                  inputEmail={this.state.inputEmail}
-                  updateInputName={this.updateInputName}
-                  updateInputEmail={this.updateInputEmail}
-                  handleCreateUsers={this.handleCreateUsers}
-              />
-              :<UsersSreen
-              userFinder={this.userFinder}
-              componentDidMount={this.componentDidMount}
-              usersList={this.state.usersList}
-              />
-              }
-            </div>
+      <div className="tag-pai">
+        {this.chooseCanvas()}
+      </div>
     )
   }
 }
-
-export default App
