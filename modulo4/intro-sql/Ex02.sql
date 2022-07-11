196d93a30e5ab9a82c856b36dcdbdb6f117f8d1d
Samuel Araujo <samuekatiom@gmail.com>
2022-07-11 19:20:18 -0300

    Intro SQL

---
+++ modulo4/intro-sql/Ex02.sql
@@ -0,0 +1,20 @@
+use
+  `aragon-samuel-araujo`;
+
+set
+  SQL_SAFE_UPDATES = 0;
+  
+INSERT INTO
+  `Employees` (id, name, email)
+VALUES
+  ("001", "Luana", "lua@lbn.com");
+
+INSERT INTO
+  `Employees` (id, name, email)
+VALUES
+  ("002", "Vinicius", "vini@lbn.com");
+
+INSERT INTO
+  `Employees` (id, name, email)
+VALUES
+  ("003", "Laura", "lau@lbn.com");
